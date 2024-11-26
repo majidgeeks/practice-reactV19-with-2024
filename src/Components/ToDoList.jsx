57cdadf7009fb063e872eee8773c87ff3c2791a5
Majid Hussain <majidrajput246@gmail.com>
2024-11-26 21:43:43 +0500

    some ui issues clear

--- src/Components/ToDoList.jsx
+++ src/Components/ToDoList.jsx
@@ -2,13 +2,13 @@ import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { MdDelete } from "react-icons/md";
 import { TiEdit } from "react-icons/ti";
-import { MdFileDownloadDone } from "react-icons/md";
+// import { MdFileDownloadDone } from "react-icons/md";
 
 const ToDoList = () => {
   const { register, handleSubmit, reset, setValue } = useForm();
   const [task, setTask] = useState([]);
   const [indexForUpdate, setIndexForUpdate] = useState(null);
-  const [lineThrough, setLineThrough] = useState(false);
+  // const [lineThrough, setLineThrough] = useState(false);
   const [dateTime, setDateTime] = useState("");
 
   const onSubmit = (data) => {
@@ -38,24 +38,26 @@ const ToDoList = () => {
     setValue("task", update[0].task);
   };
 
-  const handleLineThroghTxt = () => {
-    setLineThrough((previous) => !previous);
-  };
+  // const handleLineThroghTxt = (index) => {
+  //   let finding = task.find((curr, i) => i === index )
+  //   console.log('finding',finding.task)
+  //   // setLineThrough((previous) => !previous);
+  // };
 
   const handleClear = () => {
     setTask([]);
   }
 
   useEffect(() => {
-    const interval = () => {
+    // const interval = () => {
       setInterval(() => {
         const today = new Date();
         const formattedDate = today.toLocaleDateString();
-        const formattedTime = today.toLocaleTimeString();
-        setDateTime(`${formattedDate} - ${formattedTime}`);
+        // const formattedTime = today.toLocaleTimeString();
+        setDateTime(`${formattedDate} `);
       }, 1000);
-    };
-    return clearInterval(interval);
+    // };
+    // return clearInterval(interval);
   }, []);
 
   console.log("task in array", task);
@@ -65,21 +67,21 @@ const ToDoList = () => {
       <h2 className="text-center text-white font-bold">Create To Do List</h2>
       <h2 className="text-center text-white">{dateTime}</h2>
       <div className=" flex flex-col justify-center items-center h-full mt-2">
-        <div className="flex w-80 rounded-lg h-10  ">
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <input
-              type="text"
-              className="p-1 px-2 w-56 rounded-l-md"
-              {...register("task", { required: true })}
-            />
-            <button
-              type="submit"
-              className="bg-blue-300 px-4 py-1 rounded-r-md "
-            >
-              Add Task
-            </button>
-          </form>
-        </div>
+      <div className="flex justify-center w-96 rounded-lg h-10  ">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            type="text"
+            className="p-1 px-2 w-56 rounded-l-md"
+            {...register("task", { required: true })}
+          />
+          <button
+            type="submit"
+            className="bg-blue-300 px-4 py-1 rounded-r-md "
+          >
+            Add Task
+          </button>
+        </form>
+      </div>
         <h3 className="text-white mt-1">Excisting Tasks</h3>
         {task.map((data, index) => {
           return (
@@ -98,9 +100,9 @@ const ToDoList = () => {
                 <button onClick={() => handleDelete(index)}>
                   <MdDelete size={25} color="red" />
                 </button>
-                <button onClick={handleLineThroghTxt}>
-                  <MdFileDownloadDone size={25} color="red" />
-                </button>
+                {/* <button onClick={() => handleLineThroghTxt(index)}>
+                  <MdFileDownloadDone size={25} color="black" />
+                </button> */}
               </div>
             </div>
           );
