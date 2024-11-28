ce37935ca03463c6c9e6bd332ce3345c9d331c1a
Majid Hussain <majidrajput246@gmail.com>
2024-11-29 00:28:17 +0500

    made counter conditionally

--- src/Components/Hooks/UseState/Counter.jsx
+++ src/Components/Hooks/UseState/Counter.jsx
@@ -2,13 +2,35 @@ import { useState } from "react";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
+  const [inputValue, setInputValue] = useState(0);
+
   const increment = () => {
-    setCount(count + 1);
+    if(inputValue > 0){
+        setCount(count + inputValue <= 100 ? count + inputValue : count  )
+    }else{
+        setCount(count <= 99 ? count + 1 : count);  
+    }
   };
 
   const decrement = () => {
-    count > 0 ? setCount(count - 1) : null;
+    if(inputValue >= 1 ){
+        setCount(count - inputValue)
+    }
+    else{
+        setCount(count >= 1 ? count - 1 : null);
+    }
+    // count > 0 ? setCount(count - 1) : null;
   };
+
+  const onInputChange = (e) => {
+    setInputValue(Number(e.target.value))
+  }
+
+  const handleReset = () => {
+   setCount(0);
+  }
+
+  console.log('inputValue',inputValue)
 
   return (
     <div className="w-full h-5/6 bg-yellow-100 flex justify-center items-center">
@@ -16,12 +38,17 @@ const Counter = () => {
     <div className="flex flex-col items-center justify-center h-96 w-96 bg-blue-300">
       <h2>Count</h2>
       <h2 className="mt-5">{count}</h2>
-      <div className="flex justify-between mt-5 w-52">
-        <button className="bg-green-400 p-3 rounded-xl" onClick={increment}>
+      <input type="number" value={inputValue} onChange={(e) => onInputChange(e)}/>
+      <div className="flex justify-between mt-5 w-72">
+        <button
+         className="bg-green-400 p-3 rounded-xl" onClick={increment}>
           Increment
         </button>
         <button className="bg-red-400 p-3 rounded-xl" onClick={decrement}>
           Decrement
+        </button>
+        <button className="bg-red-400 p-3 rounded-xl" onClick={handleReset}>
+          Reset
         </button>
       </div>
     </div>
