c52dc07586594ec1646dbb232845c3fc04319bc5
Majid Hussain <majidrajput246@gmail.com>
2024-12-04 12:08:37 +0500

    layout changes in custom input

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,14 +1,31 @@
 import CustomInput from "./Utills/CustomInput";
+import { useForm, Controller } from "react-hook-form";
 
 const Register = () => {
-    return(
-         <div className="bg-blue-200 h-screen w-full">
-            <div>
-            <h1>Register Form</h1>
-            <CustomInput label={'First Name'} />
-            </div>
-         </div>
-    )
+  const { control, handleSubmit } = useForm({
+    defaultValues: {
+      firstNamr: "",
+      lastName: "",
+      contact: "",
+      password: "",
+    },
+  });
+  return (
+    <div className="bg-blue-200 h-screen w-full flex justify-center items-center ">
+      <div className="w-96 h-96 border-2 flex flex-col justify-center bg-gradient-to-r from-red-300 to-indigo-400 rounded-lg">
+        <h1 className="text-center font-medium">Register Form</h1>
+        <div className="mt-2 flex flex-col justify-evenly h-60">
+          <CustomInput inputStyle={'mt-2'} label={"First Name"} labelStyle={'font-medium text-white'} />
+          <CustomInput label={"Last Name"} labelStyle={'font-medium text-white'} />
+          <CustomInput label={"Contact"} labelStyle={'font-medium text-white'} />
+          <CustomInput label={"Password"} labelStyle={'font-medium text-white'} />
+          <div className="flex justify-center mt-5">
+          <button className="bg-white p-1 w-24 font-medium rounded-md ">Submit</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
--- src/Components/Utills/CustomInput.jsx
+++ src/Components/Utills/CustomInput.jsx
@@ -2,11 +2,11 @@ import { useForm } from "react-hook-form";
 
 
 
-const CustomInput = ({label, onChange, name,}) => {
+const CustomInput = ({label, onChange, name, labelStyle, inputStyle}) => {
     return(
-        <div>
-          {label && <label htmlFor="">{label}</label>}
-           <input type="text" name={name} onChange={onChange} />
+        <div className="flex justify-around ">
+          {label && <label htmlFor="" className={labelStyle}>{label}</label>}
+           <input type="text" name={name} onChange={onChange} className={`pr-2 pl-2 ${inputStyle}`} />
         </div>
     )
 };
