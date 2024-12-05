45a6b8273e99770c00301ead7a5f3ad5d4929dbd
Majid Hussain <majidrajput246@gmail.com>
2024-12-05 12:04:48 +0500

    created register form with RHF and set in localStorage

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -2,25 +2,120 @@ import CustomInput from "./Utills/CustomInput";
 import { useForm, Controller } from "react-hook-form";
 
 const Register = () => {
-  const { control, handleSubmit } = useForm({
+  const { control, handleSubmit, reset } = useForm({
     defaultValues: {
-      firstNamr: "",
+      firstName: "",
       lastName: "",
       contact: "",
       password: "",
     },
   });
+
+  const onSubmit = (data) => {
+    console.log("data", data);
+     localStorage.setItem('user', JSON.stringify(data))
+     const getData = JSON.parse(localStorage.getItem('user'))
+     console.log('getData',getData)
+    reset();
+  };
+
   return (
     <div className="bg-blue-200 h-screen w-full flex justify-center items-center ">
       <div className="w-96 h-96 border-2 flex flex-col justify-center bg-gradient-to-r from-red-300 to-indigo-400 rounded-lg">
         <h1 className="text-center font-medium">Register Form</h1>
         <div className="mt-2 flex flex-col justify-evenly h-60">
-          <CustomInput inputStyle={'mt-2'} label={"First Name"} labelStyle={'font-medium text-white'} />
-          <CustomInput label={"Last Name"} labelStyle={'font-medium text-white'} />
-          <CustomInput label={"Contact"} labelStyle={'font-medium text-white'} />
-          <CustomInput label={"Password"} labelStyle={'font-medium text-white'} />
+          <Controller
+            name="firstName"
+            control={control}
+            rules={{ required: "First name is required" }}
+            render={({ field: { onChange, value }, formState: { errors } }) => (
+              <>
+                <CustomInput
+                  onChange={onChange}
+                  value={value}
+                  inputStyle={"mt-2"}
+                  label={"First Name"}
+                  labelStyle={"font-medium text-white"}
+                />
+                {errors.firstName && (
+                  <span className=" text-red-600">
+                    {errors.firstName.message}
+                  </span>
+                )}
+              </>
+            )}
+          />
+
+          <Controller
+            name="lastName"
+            control={control}
+            rules={{ required: "Last name is required" }}
+            render={({ field: { onChange, value }, formState: { errors } }) => (
+              <>
+                <CustomInput
+                  onChange={onChange}
+                  value={value}
+                  inputStyle={"mt-2"}
+                  label={"Last Name"}
+                  labelStyle={"font-medium text-white"}
+                />
+                {errors.lastName && (
+                  <span className=" text-red-600">
+                    {errors.lastName.message}
+                  </span>
+                )}
+              </>
+            )}
+          />
+          <Controller
+            name="contact"
+            control={control}
+            rules={{ required: "Contact is required" }}
+            render={({ field: { onChange, value }, formState: { errors } }) => (
+              <>
+                <CustomInput
+                  onChange={onChange}
+                  value={value}
+                  inputStyle={"mt-2"}
+                  label={"Contact"}
+                  labelStyle={"font-medium text-white"}
+                />
+                {errors.contact && (
+                  <span className=" text-red-600">
+                    {errors.contact.message}
+                  </span>
+                )}
+              </>
+            )}
+          />
+          <Controller
+            name="password"
+            control={control}
+            rules={{ required: "Password is required" }}
+            render={({ field: { onChange, value }, formState: { errors } }) => (
+              <>
+                <CustomInput
+                  onChange={onChange}
+                  value={value}
+                  inputStyle={"mt-2"}
+                  label={"Password"}
+                  labelStyle={"font-medium text-white"}
+                />
+                {errors.password && (
+                  <span className=" text-red-600">
+                    {errors.password.message}
+                  </span>
+                )}
+              </>
+            )}
+          />
           <div className="flex justify-center mt-5">
-          <button className="bg-white p-1 w-24 font-medium rounded-md ">Submit</button>
+            <button
+              onClick={handleSubmit(onSubmit)}
+              className="bg-white p-1 w-24 font-medium rounded-md "
+            >
+              Submit
+            </button>
           </div>
         </div>
       </div>
--- src/Components/Utills/CustomInput.jsx
+++ src/Components/Utills/CustomInput.jsx
@@ -1,12 +1,11 @@
-import { useForm } from "react-hook-form";
 
-
-
-const CustomInput = ({label, onChange, name, labelStyle, inputStyle}) => {
+// eslint-disable-next-line react/prop-types
+const CustomInput = ({label, onChange, name, labelStyle, inputStyle, value}) => {
     return(
-        <div className="flex justify-around ">
-          {label && <label htmlFor="" className={labelStyle}>{label}</label>}
-           <input type="text" name={name} onChange={onChange} className={`pr-2 pl-2 ${inputStyle}`} />
+        <div className="flex justify-evenly  ">
+          {label && <label htmlFor="" className={`w-20 ${labelStyle}`}>{label}</label>}
+           <input type="text" value={value}
+            name={name} onChange={onChange} className={`pr-2 pl-2 ${inputStyle}`} />
         </div>
     )
 };
